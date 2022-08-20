80b6256109e4f2c6a4dcb936e9de8c2a3024b83a
ma022800 <marco.vargas@intelbras.com.br>
2022-08-20 11:16:03 -0300

    comentado erros não usados

--- src/entity/error.go
+++ src/entity/error.go
@@ -11,11 +11,11 @@ var ErrInvalidEntity = errors.New("Invalid entity")
 //ErrCannotBeDeleted cannot be deleted
 var ErrCannotBeDeleted = errors.New("Cannot Be Deleted")
 
-//ErrNotEnoughBooks cannot borrow
-var ErrNotEnoughBooks = errors.New("Not enough books")
+// //ErrNotEnoughBooks cannot borrow
+// var ErrNotEnoughBooks = errors.New("Not enough books")
 
-//ErrBookAlreadyBorrowed cannot borrow
-var ErrBookAlreadyBorrowed = errors.New("Book already borrowed")
+// //ErrBookAlreadyBorrowed cannot borrow
+// var ErrBookAlreadyBorrowed = errors.New("Book already borrowed")
 
-//ErrBookNotBorrowed cannot return
-var ErrBookNotBorrowed = errors.New("Book not borrowed")
+// //ErrBookNotBorrowed cannot return
+// var ErrBookNotBorrowed = errors.New("Book not borrowed")
