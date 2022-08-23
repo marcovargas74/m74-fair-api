a3cee446352029062ecc234f88da825d56899e0b
ma022800 <marco.vargas@intelbras.com.br>
2022-08-22 22:34:58 -0300

    criado novas mensagens de erro

--- src/entity/error.go
+++ src/entity/error.go
@@ -3,19 +3,25 @@ package entity
 import "errors"
 
 //ErrNotFound not found
-var ErrNotFound = errors.New("Not found")
+var ErrNotFound = errors.New("ERR: Not found")
 
 //ErrInvalidEntity invalid entity
-var ErrInvalidEntity = errors.New("Invalid entity")
+var ErrInvalidEntity = errors.New("ERR: Invalid entity")
 
 //ErrCannotBeDeleted cannot be deleted
-var ErrCannotBeDeleted = errors.New("Cannot Be Deleted")
+var ErrCannotBeDeleted = errors.New("ERR: Cannot Be Deleted")
+
+//ErrCannotBeUpdated cannot be deleted
+var ErrCannotBeUpdated = errors.New("ERR: Cannot Be Updated")
 
 //ErrNotExist not found
-var ErrNotExist = errors.New("Not Exist")
+var ErrNotExist = errors.New("ERR: Not Exist")
 
 //ErrIsEmpty not found
-var ErrIsEmpty = errors.New("is Empty")
+var ErrIsEmpty = errors.New("ERR: is Empty")
 
 //ErrCannotConvertJSON Cannot Convert to Json Format
-var ErrCannotConvertJSON = errors.New("Cannot Convert to Json Format")
+var ErrCannotConvertJSON = errors.New("JSON: Cannot Convert to Json Format")
+
+//ErrIsEmpty not found
+var ErrInvalidID = errors.New("ID: Invalid")
