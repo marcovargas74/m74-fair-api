f2859c21b38201c6878a7cd67b7f4f9dd5abc44a
ma022800 <marco.vargas@intelbras.com.br>
2022-08-25 23:46:46 -0300

    incluido mensagens de logs

--- src/usecase/fair/service.go
+++ src/usecase/fair/service.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/marcovargas74/m74-fair-api/src/entity"
+	"github.com/marcovargas74/m74-fair-api/src/infrastructure/logs"
 )
 
 //Service fair usecase
@@ -30,14 +31,19 @@ func (s *Service) CreateFair(name string, district string, region5 string, neigh
 
 //GetFair get a Fair
 func (s *Service) GetFair(id entity.ID) (*entity.Fair, error) {
+
+	logs.Debug("getFair(1.SERVICE)ID %s", id)
 	b, err := s.repo.Get(id)
 	if b == nil {
+		logs.Error("getFairERROR(1.SERVICE)ID %s", id)
 		return nil, entity.ErrNotFound
 	}
 	if err != nil {
+		logs.Error("getFairERROR(1.SERVICE)ID %s", id)
 		return nil, err
 	}
 
+	logs.Debug("getFair(OK.SERVICE)ID %s", id)
 	return b, nil
 }
 
@@ -57,11 +63,13 @@ func (s *Service) SearchFairs(key string, value string) ([]*entity.Fair, error) 
 func (s *Service) ListFairs() ([]*entity.Fair, error) {
 	fairs, err := s.repo.List()
 	if err != nil {
+		logs.Error("SEESSSSSER 11111 FALHA ao conectar ao Banco Mysql do DOcker %v", err)
 		return nil, err
 	}
-	if len(fairs) == 0 {
+	/*if len(fairs) == 0 {
+		logs.Error("SEESSSSSER tabale vazia %v", err)
 		return nil, entity.ErrNotFound
-	}
+	}*/
 	return fairs, nil
 }
 
