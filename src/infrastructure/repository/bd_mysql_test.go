0b2e2f408911931a3f134f955581300b4a680bdf
ma022800 <marco.vargas@intelbras.com.br>
2022-08-29 19:55:28 -0300

    Testes: Incluido arquivos de teste

---
+++ src/infrastructure/repository/bd_mysql_test.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestDBMySQL(t *testing.T) {
+	assert.Equal(t, "TODO", "TODO")
+}
---
+++ src/infrastructure/repository/fair_mysql_test.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestFairMySQL(t *testing.T) {
+	assert.Equal(t, "TODO", "TODO")
+}
